b0e3b0ef0311cce523cb16e8809f3044afb2fa1b
nestorj <nestorj@lafayette.edu>
2023-05-24 13:46:37 -0400

    oops, forgot to safe tb

---
+++ pd_to_bpm_tb.sv
@@ -0,0 +1,14 @@
+module pd_to_bpm_tb;
+
+    logic [7:0] pd, bpm;
+
+    pd_to_bpm DUV (.pd, .bpm);
+
+    initial begin
+        for (int i=0; i<=255; i++) begin
+            pd = i;
+            #5;
+        end
+    end
+
+endmodule
