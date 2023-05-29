1b4c985ee166a47cf1bb6f16bd5db2c181c7a87e
otienom <otienom@lafayette.edu>
2023-05-29 10:27:18 -0400

    instantiated a new module avg_4 , changed the internal wire to the single pulser to pulse_sp

--- sv/pulse_monitor.sv
+++ sv/pulse_monitor.sv
@@ -1,5 +1,3 @@
-//-----------------------------------------------------------------------------
-// Title         : pulse_monitor - period-based pulse monitor
 // Project       : ECE 211 - Digital Circuits 1
 //-----------------------------------------------------------------------------
 // File          : counter_bcd.sv
@@ -19,31 +17,31 @@
 module pulse_monitor (input logic clk, rst, pulse_in,
 		      output logic [3:0] d2, d1, d0);
 
-   logic [5:0] 				count, count1, count2, count3;
-   logic [7:0] 				countsum;
+   logic [5:0] 				count, count1, count2, count3, count4;
+   logic [7:0] 				bpm;
    logic [7:0]              pulseavg;
-   logic                    pulse_db, pulse_pulse;
+   logic                    pulse_db, pulse_sp, cy; // added internal wire cy connected to the carry output of bcd counter
 
-   delay_counter U_TIMER (.clk, .rst, .delay_start(1'b0), .delay_done);  // periodic 5 second delay
-
+   //delay_counter U_TIMER (.clk, .rst, .delay_start(1'b0), .pulse_sp);  // periodic 5 second delay
+  
    debounce U_PDB(.clk, .pb(pulse_in), .pb_debounced(pulse_db));
-   single_pulser U_PULSE (.clk, .din(pulse_db), .d_pulse(pulse_pulse));
    
-   // counter for pulse monitor
+   single_pulser U_PULSE (.clk, .din(pulse_db), .d_pulse(pulse_sp)); // delivers a single pulse to clear the saturating pulse counter
+   
+   pd_counter U_COUNT (.clk, .rst, .clr(pulse_sp), .enb(cy), .q(count)); // an instance of the saturating pulse counter for the pulse monitor
+  
+   reg_enb U_REG1 (.clk, .rst, .enb(pulse_sp), .d(count), .q(count1));
+   reg_enb U_REG2 (.clk, .rst, .enb(pulse_sp), .d(count1), .q(count2));
+   reg_enb U_REG3 (.clk, .rst, .enb(pulse_sp), .d(count2), .q(count3));
+   reg_enb U_REG4 (.clk, .rst, .enb(pulse_sp), .d(count3), .q(count4));
+  
+   bcd_counter U_BCDCOUNTER(.enb(1'b1),.carry(cy),.clk,.rst);
+  
+   avg_4 U_AVERAGE(.count1, .count2, .count3,.count4, .pulseavg); // adds the four registered counts and computes the average
+  
+   pd_to_bpm U_PD_TO_BPM (.pd(pulseavg), .bpm(bpm));
+  
+   binary_to_bcd U_B2B (.b(bpm), .hundreds(d2), .tens(d1), .ones(d0));
 
-   pulsecounter U_PCOUNT (.clk, .rst(delay_done), .cten(pulse_pulse), .q(count));
-   
-   reg6_enb U_REG1 (.clk, .rst, .lden(delay_done), .d(count), .q(count1));
-   reg6_enb U_REG2 (.clk, .rst, .lden(delay_done), .d(count1), .q(count2));
-   reg6_enb U_REG3 (.clk, .rst, .lden(delay_done), .d(count2), .q(count3));
-   
-   assign countsum = count1 + count2 + count3;
-
-   assign pulseavg = countsum << 2;  // multiply by 4
-
-   binary_to_bcd U_B2B (.b(pulseavg), .hundreds(d2), .tens(d1), .ones(d0));
-
-   
-   
    
 endmodule
